aae455bae4e24797b41e1993c1ab1001f638080d
201611062 <yhkim8917@naver.com>
2016-04-04 21:49:51 +0900

    #으로 삼각형그리기 업그래이드?

---
+++ w5MainTriangleUpgrade.py
@@ -0,0 +1,15 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+size=raw_input("Right number then I'll make you triangle!! : ")
+
+size=int(size)
+
+def makeTriangle(size):
+    for i in range(size):
+        print " "*(size-i)+"#"*(i*2-1)
+
+makeTriangle(size)
+
+wn.exitonclick()
