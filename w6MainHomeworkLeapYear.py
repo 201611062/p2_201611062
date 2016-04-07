d2165fb8f92c262aa849e946f882074c8015ee33
201611062 <yhkim8917@naver.com>
2016-04-07 12:38:36 +0900

    이게 진짜 진짜 윤년 py

---
+++ w6MainHomeworkLeapYear.py
@@ -0,0 +1,7 @@
+﻿def leapYear(year):
+    if (year%4==0) and (year%100!=0 or year%400==0):
+        print "This year is leap year"
+    else:
+        print "This year is not leap year"
+
+leapYear(2100)
