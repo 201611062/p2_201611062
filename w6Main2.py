d1b537893d15310fc312351c31bdef596756c6e1
201611062 <yhkim8917@naver.com>
2016-04-11 17:11:37 +0900

    4-11 과제 제출합니다.

---
+++ w6Main2.py
@@ -0,0 +1,21 @@
+﻿def sumList(aList):
+    x=list()
+    for i in range(0,aList):
+        if i%4==0 and i%5!=0:
+            x.append(i)
+    sum=0
+    for i in range(0,len(x)):
+        sum+=x[i]
+    mysum=sum
+    return mysum
+
+def lab6():
+    "programing too hard"
+    aList=1000
+    labsum=sumList(aList)
+    print labsum
+
+def main():
+    lab6()  
+
+main()
