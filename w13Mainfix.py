1e4162b3db6c1ad481eaadd6fb776449b66bf69c
201611062 <yhkim8917@naver.com>
2016-05-24 01:29:11 +0900

    05-23과제 수정합니다. 죄송합니다.
     실행은 경로때문에 에러나는데 경로만 수정해 주시면 됩니다.

---
+++ w13Mainfix.py
@@ -0,0 +1,57 @@
+﻿
+import os
+
+def findword():
+    mydir=os.getcwd()
+    filename='writefileBasic.txt'
+    myfilename=os.path.join(mydir,filename)
+    file=raw_input("Write file name : ")
+    if filename==file:
+        #한 줄읽기    
+        myfile=open(filename)
+        line=myfile.readline()
+        myfile.close()
+        print line
+    
+        #전체읽기
+        myfile=open(filename)
+        for line in myfile:
+            print line
+        myfile.close()
+    
+        #줄 찾기
+        word=raw_input("Write word : ")
+        myfile=open(filename,'r')
+        for line in myfile:
+            if line.find(word)>=0:
+                print line
+        myfile.close()
+        
+    else:
+        print IOError
+
+    
+def changeUpper():
+    #대문자 바꾸기
+    myfile2=open('output.txt','w')
+    line1=raw_input("Write line1 : ")
+    line2=raw_input("Write line2 : ")
+    
+    myfile2.write(line1+('\n'))
+    myfile2.write(('\t')+line2)
+    
+    myfile2=open('output.txt','r') 
+    contentstwo=myfile2.readlines() 
+    for a in range(0,len(contentstwo)): 
+        print contentstwo[a].upper() 
+    myfile2.close() 
+
+def lab13():
+    findword()
+    changeUpper()
+
+def main():
+    lab13()
+
+main()
+raw_input("")
