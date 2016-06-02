fc41809d006bca57182f36f757df5e6997430d3c
201611062 <yhkim8917@naver.com>
2016-06-02 11:40:49 +0900

    06-01과제입니다.

---
+++ w14Main2.py
@@ -0,0 +1,35 @@
+﻿class Dog(object):
+    def __init__(self,name,species):
+        self.name=name
+        self.species=species
+    def dog(self):
+        print "My dog name is ",self.name+" and species is ",self.species+" and sounds mung mung"
+        
+        
+class Shihtzu(object):
+    def __init__(self,name,species):
+        self.name=name
+        self.species=species
+    def dog(self):
+        print "My dog name is ",self.name+" and species is ",self.species+" and sounds si si"
+        
+class Maltese(object):
+    def __init__(self,name,species):
+        self.name=name
+        self.species=species
+    def dog(self):
+         print "My dog name is ",self.name+" and species is ",self.species+" and sounds mal mal"
+
+dogname=raw_input("Write dog name : ")
+dogsound=raw_input("Write dog species : ")
+if dogsound=="Shihtzu":
+    answer=Shihtzu(dogname,dogsound)
+    answer.dog()
+elif dogsound=="Maltese":
+    answer=Maltese(dogname,dogsound)
+    answer.dog()
+else:
+    answer=Dog(dogname,dogsound)
+    answer.dog()
+
+raw_input("")
